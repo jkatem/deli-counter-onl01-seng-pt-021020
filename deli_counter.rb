7994e96471889cb129543628ca90e0faa9422f26
Jen Miao <jenmiao@gmail.com>
2020-02-18 22:59:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,28 @@
-let(:katz_deli) { [] }
-let(:other_deli) { ["Logan", "Avi", "Spencer"] }
-let(:another_deli) { ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"] }
-
+# let(:katz_deli) { [] }
+# let(:other_deli) { ["Logan", "Avi", "Spencer"] }
+# let(:another_deli) { ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"] }
+# 
 
 def line(arr)
   if arr = []
-    puts "The line is currently empty."
+    puts "The line is currently empty." 
   elsif condition
-
+     
   end
 end
 
 def take_a_number(arr, num)
-
+  
   puts arr.each_with_index { |name, num| greet << "Welcome, #{name}. You are number #{num+1} in line."}
 end
+  
+
+
+
+
+
+
+
+
+
+
