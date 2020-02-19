385c1cc347009e8732fed6328ed550c76247cfdd
Jen Miao <jenmiao@gmail.com>
2020-02-19 00:29:09 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,43 @@
-# Write your code here.
+# let(:katz_deli) { [] }
+# let(:other_deli) { ["Logan", "Avi", "Spencer"] }
+# let(:another_deli) { ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"] }
+#
+#
+# def line(arr)
+#   if arr = []
+#     puts "The line is currently empt."
+#   else
+#     arr.each_with_index do |name, number|
+#       puts "The line is currently: #{number}. #{name}"
+#   end
+# end
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    message = "The line is currently:"
+
+  array.each_with_index do |value, index|
+    message += " #{index.to_i+1}. #{value}"
+  end
+  puts "#{message}"
+  end
+end
+
+
+def take_a_number(array, name)
+  array.push(name)
+  position = array.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name, position
+end
+
+
+def now_serving(array)
+  if array.empty? == true
+    puts "There is nobody waiting to be served!"
+  elsif array.empty? == false
+    puts "Currently serving #{array.shift}."
+  end
+end
