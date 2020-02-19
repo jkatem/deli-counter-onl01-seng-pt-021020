89f82de6a71dab14f25ffba7ea0060242a576f4a
Jen Miao <jenmiao@gmail.com>
2020-02-19 00:25:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,16 +2,28 @@
 # let(:other_deli) { ["Logan", "Avi", "Spencer"] }
 # let(:another_deli) { ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"] }
 #
+#
+# def line(arr)
+#   if arr = []
+#     puts "The line is currently empt."
+#   else
+#     arr.each_with_index do |name, number|
+#       puts "The line is currently: #{number}. #{name}"
+#   end
+# end
 
-def line(arr)
-  if arr = []
-    puts "The line is currently empt."
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
   else
-    arr.each_with_index do |name, number|
-      puts "The line is currently: #{number}. #{name}"
-  # end
-end
+    message = "The line is currently:"
 
+  array.each_with_index do |value, index|
+    message += " #{index.to_i+1}. #{value}"
+  end
+  puts "#{message}"
+  end
+end
 
 
 # def take_a_number(arr, num)
