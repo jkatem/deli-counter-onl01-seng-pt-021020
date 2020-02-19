acf23bbcd88d5b23dc6f78fad4337388faa3c499
Jen Miao <jenmiao@gmail.com>
2020-02-19 00:28:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,6 +26,18 @@ def line(array)
 end
 
 
-def take_a_number(arr, num)
-  puts arr.each_with_index { |name, num| greet << "Welcome, #{name}. You are number #{num+1} in line."}
+def take_a_number(array, name)
+  array.push(name)
+  position = array.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name, position
+end
+
+
+def now_serving(array)
+  if array.empty? == true
+    puts "There is nobody waiting to be served!"
+  elsif array.empty? == false
+    puts "Currently serving #{array.shift}."
+  end
 end
